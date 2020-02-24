c38bd351f0387af25d2be329f28c1a58fb39b22e
Lydell Gaines <dasnic70@gmail.com>
2020-02-24 01:41:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,12 @@
 #   "Amanda" => 27
 # }
 
+require 'spec_helper'
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
 end
+
 
 
